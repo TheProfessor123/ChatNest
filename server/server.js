cad8cef013cf49f562cf941f7b36b4cf28386688
TheProfessor123 <chaganramchoudhary7@gmail.com>
2025-03-09 11:52:48 +0530

    fix: allowed all origins

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const WebSocket = require('ws');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 const upload = multer();
